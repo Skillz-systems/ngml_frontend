5b6145f00aac3e0bc311009097d7d68657cde0e4
Brisstone <okolijohnson69@gmail.com>
2023-11-04 09:57:41 +0100

    OJ-Project-Init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    //Ignore Eslint in .eslintrc.js
+    ignorePatterns: [".eslintrc.js"],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'react'],
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
