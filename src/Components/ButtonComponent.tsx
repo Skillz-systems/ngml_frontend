ed3ceee260c05485c6573b022f32a268f12a391e
Joynas <Ibinichinasajoy@gmail.com>
2023-11-09 13:11:39 +0100

    created-test-folder-and-wrote-documentation

--- src/Components/ButtonComponent.tsx
+++ src/Components/ButtonComponent.tsx
@@ -1,5 +1,21 @@
 import React, { type ReactNode, type FC } from 'react'
 
+/**
+ * Represents the Default Button Component
+ * @function
+ * @param {string} children - react props to render wrapped children component
+ * @param onClick: () => void (required) - Function to execute on button click.
+ * @param {string} backgroundColor - Background color of the button .
+ * @param {string} width - Width of the button .
+ * @param {string} border - Border style of the button .
+ * @param {string} radius - Border radius of the button .
+ * @param {string} height- Height of the button .
+ * @param {string} fontSize - Font size of the button text .
+ * @param {string} color -Text color of the button .
+ * @Description - this component is a reusable React component for creating customizable buttons and takes in the above props.
+
+ */
+
 interface ButtonComponentProps {
   children: ReactNode
   onClick: () => void
