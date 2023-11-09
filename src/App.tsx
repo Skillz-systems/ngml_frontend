29ce372c5fa2c98f8af931bf962e05335dd99357
Joynas <Ibinichinasajoy@gmail.com>
2023-11-09 23:29:22 +0100

    created-staff-login

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import ButtonComponent from './Components/ButtonComponent'
+import StaffLoginPage from './Pages/StaffLoginPage'
 
 function App (): JSX.Element {
-  const handleClick = (): void => {
-    console.log('Button clicked!')
-  }
-
   return (
     <div className="App">
-      <div>
-        <ButtonComponent
-         border="none"
-         backgroundColor="#59d14b"
-         height = "50px"
-         radius = "40px"
-         width = "500px"
-         onClick={() => {
-           handleClick()
-         }}
-        > Login</ButtonComponent>
-      </div>
-      <hr></hr>
-      <hr></hr>
-      <hr></hr>
-      <div>
-        <ButtonComponent
-         border="2px solid #91918e"
-         backgroundColor="#f4f78b"
-         height = "50px"
-         radius = "40px"
-         width = "250px"
-         color= 'black'
-         onClick={() => {
-           handleClick()
-         }}
-        > New Sign in</ButtonComponent>
-      </div>
+     <Routes>
+     <Route path="staffloginpage" element={<StaffLoginPage />} />
+     </Routes>
     </div>
   )
 }
---
+++ src/Pages/StaffLoginPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StaffLoginPage: React.FC = () => {
+  return (
+      <div className='bg-red-500 w-[100%]'>
+        <div>Go girl !!!</div>
+      </div>
+  )
+}
+
+export default StaffLoginPage
