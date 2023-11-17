963a9595655b137d364267ddef375d29f34410b6
edwin <edwinosayande@gmail.com>
2023-11-17 15:32:17 +0100

    navbarr

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { PrivateAdminRoute, routes } from './Routes/Index'
-import './App.css'
+import NavBar from './Components/NavBar'
 
 // eslint-disable-next-line @typescript-eslint/space-before-function-paren
 function App(): JSX.Element {
   // routes without layouts
-  const authRoutes = routes.AuthRoutes.map(({ path, component: Component }) => (
-    <Route key={path} path={path} element={<Component />} />
-  ))
-
-  const PrivateRoutes = routes.PrivateRoutes.map(
-    ({ path, component: Component, exact }) => (
-      <>
-        <Route
-          key={path}
-          path={path}
-          element={
-            <PrivateAdminRoute key={path} >
-              <Component />
-            </PrivateAdminRoute>
-          }
-        />
-      </>
-
-    )
-  )
 
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {authRoutes}
-          {PrivateRoutes}
-        </Routes>
-      </BrowserRouter>
+    <div className="">
+      <NavBar />
     </div>
   )
 }
