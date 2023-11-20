05875dbce739b7840f19bcb39c10ecaa03d1e2fa
Brisstone <okolijohnson69@gmail.com>
2023-11-20 12:33:13 +0100

    OJ-responsiveness

--- src/Pages/StaffPage.tsx
+++ src/Pages/StaffPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type FC } from 'react'
+import React, { useState, type FC, useEffect } from 'react'
 import Tabs from 'src/Components/Tabs/Tabs'
 import print from 'src/Asset/png-icons/print.png'
 import AltDownArrow from 'src/Asset/png-icons/AltDownArrow.png'
@@ -6,22 +6,33 @@ import AltDownArrow from 'src/Asset/png-icons/AltDownArrow.png'
 const Staffpage: FC = () => {
   const [activeTab, setActiveTab] = useState<string>('staffinformation')
 
+  console.log(activeTab, 'activeTabactiveTab')
+
+  useEffect(() => {
+
+  }, [activeTab])
+
   return (
     <div className="m-5 bg-white/40 flex-1 p-5 overflow-x-hidden rounded-xl">
       <div className="flex justify-between">
-        <h3 className="text-left capitalize font-semibold text-3xl text-neutral-600">
+        <div> <h3 className="text-left capitalize font-semibold text-3xl text-neutral-600">
           Staff page
-        </h3>
-        <div>
+        </h3></div>
+
+        <div className='flex flex-row gap-[10px]'>
           {' '}
-          <p className="px-4 py-1 border cursor-pointer hover:text-neutral-700 ease-in-out duration-300 transition-all border-neutral-400 rounded-3xl text-neutral-600">
+          <div style={{ height: '100%', justifyItems: 'center', alignItems: 'center' }} className="flex justify-center px-4 border cursor-pointer hover:text-neutral-700 ease-in-out duration-300 transition-all border-neutral-400 rounded-3xl text-neutral-600">
             Cancel
-          </p>
+          </div>
+
           {activeTab === 'summary' && (
-            <div className="justify-end items-center gap-4 flex mr-[-300px]">
-              <div className="w-8 h-8 p-2.5 rounded-[40px] border border-gray-300 flex-col justify-center items-center gap-2.5 inline-flex">
+            <div style={{ alignItems: 'center' }} className="items-center gap-4 flex">
+
+              <div className="">
+
                 <div className="w-4 h-4 justify-center items-center inline-flex">
-                  {/* <div className="w-4 h-4 relative flex-col justify-start items-start flex"></div> */}
+
+                  <div className="w-4 h-4 relative flex-col justify-start items-start flex"></div>
                   <img
                     className="w-4 h-4 relative flex-col justify-start items-start flex"
                     src={print}
