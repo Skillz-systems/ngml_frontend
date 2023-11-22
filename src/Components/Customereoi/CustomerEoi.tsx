4cce4e828b611d79340b8242171211414d64018b
Brisstone <okolijohnson69@gmail.com>
2023-11-22 13:10:16 +0100

    OJ-Fetch-customer-EOI

--- src/Components/Customereoi/CustomerEoi.tsx
+++ src/Components/Customereoi/CustomerEoi.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useDataFetcher from 'src/api/swr'
+import moment from 'moment'
 
 const CustomerEoi: React.FC = () => {
+  const { id } = useParams()
+
+  const { data, error } = useDataFetcher({ url: `/customer/eoi/${id}` })
+  console.log(error, 'errorerror')
+
   return (
     <div className="w-full h-full p-4 bg-white rounded-xl flex-col justify-start items-start gap-6 inline-flex">
       <div className="self-stretch h-full bg-white rounded-xl border border-gray-200 flex-col justify-start items-start flex">
@@ -14,8 +22,8 @@ const CustomerEoi: React.FC = () => {
         </div>
         <div className="self-stretch px-3 py-2.5 bg-lime-50 border-t border-b border-gray-200 justify-between items-center inline-flex">
           <div className="justify-start items-center gap-2.5 flex">
-            <div className="text-slate-400 text-xs font-bold font-['Mulish'] leading-3">Request Received On</div>
-            <div className="text-slate-400 text-xs font-normal font-['Mulish'] leading-3">09th, Nov, 2023; 09:23:44AM</div>
+            <div className="text-slate-400 text-xs font-bold font-['Mulish'] leading-3">Request Received On: </div>
+            <div className="text-slate-400 text-xs font-normal font-['Mulish'] leading-3">{moment(data?.data?.createdAt).format('Do, MMM, YYYY; hh:mm:ssA')}</div>
           </div>
           <div className="p-2 bg-slate-200 rounded-3xl flex-col justify-center items-center gap-2.5 inline-flex">
             <div className="text-center text-zinc-950 text-xs font-medium font-['Mulish'] leading-3 tracking-tight">New</div>
@@ -24,15 +32,15 @@ const CustomerEoi: React.FC = () => {
         <div className="self-stretch h-[94px] px-3 py-5 bg-slate-50 border-b border-gray-200 flex-col justify-start items-start gap-3 flex">
           <div className="self-stretch justify-start items-center inline-flex">
             <div className="h-2.5 justify-start items-center gap-2.5 flex">
-              <div className="text-slate-400 text-sm font-semibold font-['Mulish'] leading-[14px]">Company Name</div>
+              <div className="text-slate-400 text-sm font-semibold font-['Mulish'] leading-[14px]">Company Name:&nbsp; </div>
             </div>
-            <div className="text-slate-600 text-sm font-bold font-['Mulish'] leading-[14px]">Dangote Cement LTD.</div>
+            <div className="text-slate-600 text-sm font-bold font-['Mulish'] leading-[14px]">{data?.data?.companyName}.</div>
           </div>
           <div className="self-stretch justify-start items-center inline-flex">
             <div className="h-2.5 justify-start items-center gap-2.5 flex">
-              <div className="text-slate-400 text-sm font-semibold font-['Mulish'] leading-[14px]">Company Email</div>
+              <div className="text-slate-400 text-sm font-semibold font-['Mulish'] leading-[14px]">Company Email:&nbsp; </div>
             </div>
-            <div className="text-slate-600 text-sm font-bold font-['Mulish'] leading-[14px]">info@dangotecement.org</div>
+            <div className="text-slate-600 text-sm font-bold font-['Mulish'] leading-[14px]">{data?.data?.email}</div>
           </div>
           <div className="self-stretch justify-start items-center inline-flex">
             <div className="h-2.5 justify-start items-center gap-2.5 flex">
@@ -44,7 +52,7 @@ const CustomerEoi: React.FC = () => {
         <div className="self-stretch grow shrink basis-0 p-3 bg-white justify-start items-center gap-2.5 inline-flex">
           <div className="grow shrink basis-0 self-stretch p-5 rounded-xl border border-gray-200 flex-col justify-start items-start gap-5 inline-flex">
             <div className="self-stretch text-slate-400 text-base font-bold font-['Mulish'] leading-tight tracking-tight">REASON FOR REQUEST</div>
-            <div className="self-stretch text-zinc-950 text-base font-medium font-['Mulish'] leading-tight tracking-tight">We want gas to power our turbines in our new manufacturing site in Edo state. simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+            <div className="self-stretch text-zinc-950 text-base font-medium font-['Mulish'] leading-tight tracking-tight">{data?.data?.reason}.</div>
           </div>
         </div>
         <div className="self-stretch px-3 py-2 border-0 justify-end items-center gap-2.5 inline-flex">
